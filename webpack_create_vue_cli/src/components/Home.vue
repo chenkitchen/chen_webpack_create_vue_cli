f2effdf4c8c7ed26d9d9023eac767fe311006559
chenkitchen <1591584907@qq.com>
2022-02-02 12:21:22 +0800

    ztree

--- webpack_create_vue_cli/src/components/Home.vue
+++ webpack_create_vue_cli/src/components/Home.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         这是home <span>{{show}}</span>
+        <ul>
+            <li>点击删除下面的水果</li>
+            <li v-for="(item,index) in arr" :key=index @click="hidMy(item,arr)">{{item}}</li>
+        </ul>
     </div>
 </template>
 
@@ -9,11 +13,22 @@
         data() {
             return {
                 show:'',
+                arr:["apple","peach","banana","oriange","grape"]
             }
         },
         mounted(){
             this.show = this.$route.query.shopid
-        }
+        },
+        methods: {
+            hidMy(ele,arr){
+                arr.map((item,index)=>{
+                    if(ele === item){
+                        arr.splice(index,1);
+                        alert(item);
+                    }
+                })
+            }
+        },
     }
 </script>
 
