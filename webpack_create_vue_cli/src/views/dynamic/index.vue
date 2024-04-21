01ce4c811c9d2e3c158bc7598458ba597565f562
chenkitchen <1591584907@qq.com>
2024-04-21 17:55:21 +0800

    常见功能

--- webpack_create_vue_cli/src/views/dynamic/index.vue
+++ webpack_create_vue_cli/src/views/dynamic/index.vue
@@ -19,6 +19,15 @@
             <el-card class="card" :body-style="{ padding: '10px' }">
                 <div id="waveform" ref="waveform">
                 </div>
+                <audio
+                    src="http://127.0.0.1:5503/staticResource/files/gr_syn138_woods_Dm.wav"
+                    controls
+                    class="audio4"
+                    @play="play"
+                    @pause="pause"
+                    @seeking="seeking"
+                    controlsList=" nodownload"
+                ></audio>
             </el-card>
         </el-row>
         <div>
@@ -107,6 +116,8 @@ const getToolbar = () => ({
   toolbarViewerMiddle: getToolbarViewerMiddle(),
 });
 
+
+
 export default {
     components: {
     VuePdfApp
@@ -137,22 +148,81 @@ export default {
         this.wavesurfer.playPause.bind(this.wavesurfer)();
     },
     newAudio(){
+        const options = {
+        /** HTML element or CSS selector (required) */
+        container: this.$refs.waveform,
+        /** The height of the waveform in pixels */
+        height: 128,
+        /** The width of the waveform in pixels or any CSS value; defaults to 100% */
+        width: 500,
+        /** Render each audio channel as a separate waveform */
+        splitChannels: false,
+        /** Stretch the waveform to the full height */
+        normalize: false,
+        /** The color of the waveform */
+        waveColor: '#ff4e00',
+        /** The color of the progress mask */
+        progressColor: '#dd5e98',
+        /** The color of the playpack cursor */
+        cursorColor: '#ddd5e9',
+        /** The cursor width */
+        cursorWidth: 2,
+        /** Render the waveform with bars like this: ▁ ▂ ▇ ▃ ▅ ▂ */
+        barWidth: NaN,
+        /** Spacing between bars in pixels */
+        barGap: NaN,
+        /** Rounded borders for bars */
+        barRadius: NaN,
+        /** A vertical scaling factor for the waveform */
+        barHeight: NaN,
+        /** Vertical bar alignment **/
+        barAlign: '',
+        /** Minimum pixels per second of audio (i.e. zoom level) */
+        minPxPerSec: 1,
+        /** Stretch the waveform to fill the container, true by default */
+        fillParent: true,
+        /** Audio URL */
+        // url: '/examples/audio/audio.wav',
+        /** Whether to show default audio element controls */
+        mediaControls: true,
+        /** Play the audio on load */
+        autoplay: false,
+        /** Pass false to disable clicks on the waveform */
+        interact: true,
+        /** Allow to drag the cursor to seek to a new position */
+        dragToSeek: false,
+        /** Hide the scrollbar */
+        hideScrollbar: false,
+        /** Audio rate */
+        audioRate: 1,
+        /** Automatically scroll the container to keep the current position in viewport */
+        autoScroll: true,
+        /** If autoScroll is enabled, keep the cursor in the center of the waveform during playback */
+        autoCenter: true,
+        /** Decoding sample rate. Doesn't affect the playback. Defaults to 8000 */
+        sampleRate: 8000,
+        }
         this.$nextTick(() => {
-        this.wavesurfer = WaveSurfer.create({
-            container: this.$refs.waveform,
-            // waveColor: '#409EFF',
-            barWidth: 1,
-            cursorColor: "black",
-            progressColor: "blue",
-            backend: "MediaElement",
-            // mediaControls: false,
-            audioRate: "1",
-            //使用时间轴插件
-            plugins: [Timeline.create()]
-        });
+        // this.wavesurfer = WaveSurfer.create({
+        //     container: this.$refs.waveform,
+        //     // waveColor: '#409EFF',
+        //     barWidth: 1,
+        //     cursorColor: "black",
+        //     progressColor: "blue",
+        //     backend: "MediaElement",
+        //     // mediaControls: false,
+        //     audioRate: "1",
+        //     //使用时间轴插件
+        //     plugins: [Timeline.create()]
+        // });
+        this.wavesurfer = WaveSurfer.create(options)
         // 特别提醒：此处需要使用require(相对路径)，否则会报错
         this.wavesurfer.load("http://127.0.0.1:5503/staticResource/files/gr_syn138_woods_Dm.wav");
         });
+
+        this.wavesurfer.on('ready', () => {
+            wavesurfer.setTime(10)
+        })
     },
     t(key) {
     //   return this.messages[key];
